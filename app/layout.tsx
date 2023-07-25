c0e8125cdf69fa3b7ab6dfd51ad7edb96a60d06d
ASTERISK Kwon <hello@asterisk.moe>
2023-07-25 05:38:30 +0000

    feat: 레이아웃 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,70 @@
 import '@/styles/globals.css'
+import { cookies } from 'next/headers';
 import { Analytics } from '@vercel/analytics/react';
+import DefaultFooter from '@/components/layouts/default/DefaultFooter';
+import DefaultHeader from '@/components/layouts/default/DefaultHeader';
+import type { IdentityType, StreamInfoType } from 'type';
 
-export default function RootLayout({ children }: {
+async function GetIdentity() {
+  const authorization = cookies().get('authorization');
+  if (!authorization) return undefined;
+
+  const res = await fetch('https://dev.naseong.kim/api/user/detail', {
+    headers: {
+      Cookie: `Authorization=${authorization.value}`
+    }
+  });
+
+  if(!res.ok) return undefined;
+  return res.json() as Promise<IdentityType>;
+}
+
+async function GetTwitchAccessToken() {
+  const res = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, {
+    next: { revalidate: 5000000 },
+    method: 'POST'
+  })
+
+  if (!res.ok) return undefined;
+  return res.json() as Promise<{ access_token: string }>;
+}
+
+async function GetStream() {
+  const appAccessToken = await GetTwitchAccessToken();
+  if(!appAccessToken || !process.env.TWITCH_BROADCASTER_ID || !process.env.TWITCH_CLIENT_ID) return undefined;
+
+  const res = await fetch(`https://api.twitch.tv/helix/streams?user_id=${process.env.TWITCH_BROADCASTER_ID}`, {
+    next: { revalidate: 300 },
+    headers: {
+      'Authorization': `Bearer ${appAccessToken?.access_token}`,
+      'Client-Id': process.env.TWITCH_CLIENT_ID
+    }
+  })
+
+  if (!res.ok) return undefined;
+  return res.json() as Promise<StreamInfoType>;
+}
+
+export default async function RootLayout({ children }: {
   children: React.ReactNode
 }) {
-return (
+  const streamData = GetStream();
+  const IdentityData = GetIdentity();
+  
+  const [stream, identity] = await Promise.all([streamData, IdentityData]);
+  return (
     <html lang="ko">
       <body>
-        {children}
+        <main>
+          <DefaultHeader
+            stream={stream?.data[0]}
+            me={identity}
+          />
+          <div className="py-10">
+            {children}
+          </div>
+          <DefaultFooter />
+        </main>
         <Analytics />
       </body>
     </html>
