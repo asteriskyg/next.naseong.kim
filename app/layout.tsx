61be302dac8ef96b02c6b5fb8e1b89068f7653ad
ASTERISK Kwon <hello@asterisk.moe>
2023-07-25 05:24:22 +0000

    feat: Vercel Web Analytics 마이그레이션

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({ children }: {
   children: React.ReactNode
 }) {
 return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
