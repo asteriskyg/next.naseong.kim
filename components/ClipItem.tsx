0c22113ca028dc0cdbad399eb4ec62dfc9485996
ASTERISK Kwon <hello@asterisk.moe>
2023-07-25 12:32:49 +0000

    feat: 시간 표시 가독성 개선

--- components/ClipItem.tsx
+++ components/ClipItem.tsx
@@ -9,29 +9,27 @@ dayjs.extend(relativeTime);
 export default function ClipItem({ clip }: { clip: Clip }) {
   return (
     <Link
-      href={`/detail/${clip.contentId}`}
-      className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow"
+      href={`/detail/${clip.clipName}`}
+      className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white hover:bg-stone-100 hover:shadow-lg border transition-all"
     >
-      <div>
-        <div className="relative aspect-video bg-slate-100 dark:bg-neutral-800">
-          <Image
-            src={`https://customer-lsoi5zwkd51of53g.cloudflarestream.com/${clip.contentId}/thumbnails/thumbnail.jpg`}
-            alt=""
-            width={640}
-            height={360}
-          />
-          <div className="absolute bottom-2 right-2 flex h-6 w-11 items-center justify-center rounded-lg bg-twitch-dark/60 text-sm text-white backdrop-blur-md">
-            { 
-              clip.clipDuration > 59
-                ? clip.clipDuration - 60 < 10
-                  ? `1:0${clip.clipDuration - 60}`
-                  : `1:${clip.clipDuration - 60}`
-                : clip.clipDuration < 10
-                  ? `0:0${clip.clipDuration}`
-                  : `0:${clip.clipDuration}`
-              }
-          </div>
-        </div>
+      <div className="relative bg-slate-100 dark:bg-neutral-800">
+        <Image
+          src={`https://customer-lsoi5zwkd51of53g.cloudflarestream.com/${clip.contentId}/thumbnails/thumbnail.jpg`}
+          alt=""
+          width={640}
+          height={360}
+        />
+        <span className="absolute bottom-1 right-1 inline-flex items-center rounded-lg bg-gray-50/90 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+          {
+            clip.clipDuration > 59
+              ? clip.clipDuration - 60 < 10
+                ? `1:0${clip.clipDuration - 60}`
+                : `1:${clip.clipDuration - 60}`
+              : clip.clipDuration < 10
+                ? `0:0${clip.clipDuration}`
+                : `0:${clip.clipDuration}`
+            }
+        </span>
       </div>
       <div className="px-4 py-4 sm:px-6">
       <div className=" text-black dark:text-slate-200">
