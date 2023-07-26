c8cfc598f9c6a71147c1bd90be420b69aaa4db68
ASTERISK Kwon <hello@asterisk.moe>
2023-07-26 12:05:31 +0000

    feat: 프로필 페이지 추가

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'customer-lsoi5zwkd51of53g.cloudflarestream.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.naseong.kim',
+      },
     ],
   },
 }
