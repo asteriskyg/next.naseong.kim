5177419a763942ff9201574f9dcba7ec45cf36cb
ASTERISK Kwon <hello@asterisk.moe>
2023-07-27 15:41:11 +0000

    feat: storybook 추가

---
+++ stories/ClipItem.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ClipItem from '../components/ClipItem';
+
+const meta: Meta<typeof ClipItem> = {
+  title: 'ClipItem',
+  component: ClipItem,
+  // ...
+};
+    
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const withClip: Story = {
+  args: {
+    clip: {
+      clipCreatedAt: new Date(),
+      clipDuration: 90,
+      clipLastEdited: new Date(),
+      contentId: 'string',
+      contentName: 'string',
+      creatorId: 1,
+      creatorName: 'string',
+      gameId: 1,
+      gameName: 'string',
+      streamStartedAt: new Date(),
+      clipName: 'string',
+    },
+  }
+  };
