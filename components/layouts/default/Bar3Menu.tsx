419af74141862f19be382c668bcf654234ffe820
ASTERISK Kwon <hello@asterisk.moe>
2023-08-17 18:00:25 +0900

    feat: 모바일 햄버거 제거

--- components/layouts/default/Bar3Menu.tsx
+++ components/layouts/default/Bar3Menu.tsx
@@ -1,31 +1,43 @@
-import Image from 'next/image'
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid'
-import { Bars3Icon, LockClosedIcon } from '@heroicons/react/24/outline'
-import type { IdentityType } from 'type';
-import VerticalNavigation from '@/components/tailwind/Navigation/VerticalNavigation'
-import CustomIcon from '@/components/CustomIcon';
-import PopoverButton from '@/components/tailwind/PopoverButton';
+"use client";
+import Image from "next/image";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, LockClosedIcon } from "@heroicons/react/24/outline";
+import type { IdentityType } from "type";
+import VerticalNavigation from "@/components/tailwind/Navigation/VerticalNavigation";
+import CustomIcon from "@/components/CustomIcon";
+import PopoverButton from "@/components/tailwind/PopoverButton";
 
 const TwitchIcon = () => {
   return (
     <CustomIcon className="w-6 h-6 text-gray-600 group-hover:text-indigo-600">
-      <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z" clipRule="evenodd" />
+      <path
+        d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z"
+        clipRule="evenodd"
+      />
     </CustomIcon>
-  )
-}
+  );
+};
 
 const lists = [
-  { name: '김나성 트위치', href: 'https://twitch.tv/naseongkim', icon: TwitchIcon },
-  { name: '김나성 트게더', href: 'https://tgd.kr/s/naseongkim', icon: ChatBubbleLeftRightIcon }
-]
+  {
+    name: "김나성 트위치",
+    href: "https://twitch.tv/naseongkim",
+    icon: TwitchIcon,
+  },
+  {
+    name: "김나성 트게더",
+    href: "https://tgd.kr/s/naseongkim",
+    icon: ChatBubbleLeftRightIcon,
+  },
+];
 
 export default function MenuComponent({ me }: { me: IdentityType | undefined }) {
   if(!me) {
     return (
       <Popover className="relative flex flex-col">
-        <Popover.Button className="inline-flex items-center justify-end gap-x-1 text-sm font-semibold leading-6 text-gray-900 h-8 w-8">
+        <Popover.Button className="gap-x-1 text-sm font-semibold leading-6 text-gray-900">
           <Bars3Icon className="shrink-0 h-6 w-6 text-gray-600" aria-hidden="true" />
         </Popover.Button>
         <Transition
@@ -35,39 +47,44 @@ export default function MenuComponent({ me }: { me: IdentityType | undefined }) 
           enterTo="opacity-100 translate-y-0"
           leave="transition ease-in duration-150"
           leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
-        >
-        <Popover.Panel className="absolute right-0 mt-10 w-screen max-w-sm">
-          <div className="w-full overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            <div className="p-4 border-b">
-            <PopoverButton
-              button={{
-                name: '트위치로 로그인',
-                description: '로그인 하고 클립 만들기',
-                href: `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=0373yf8vzqpo4f9ln4ajqrq9fim3hd&redirect_uri=${process.env.NEXT_PUBLIC_APP_PROTOCOL}://${process.env.NEXT_PUBLIC_APP_HOST}/api/authorization&scope=clips%3Aedit%20user%3Aread%3Aemail%20user%3Aread%3Asubscriptions`,
-                icon: LockClosedIcon
-              }}
-            />
+          leaveTo="opacity-0 translate-y-1">
+          <Popover.Panel className="absolute right-0 mt-10 w-screen max-w-sm">
+            <div className="w-full overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+              <div className="p-4 border-b">
+                <PopoverButton
+                  button={{
+                    name: "트위치로 로그인",
+                    description: "로그인 하고 클립 만들기",
+                    href: `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=0373yf8vzqpo4f9ln4ajqrq9fim3hd&redirect_uri=${process.env.NEXT_PUBLIC_APP_PROTOCOL}://${process.env.NEXT_PUBLIC_APP_HOST}/api/authorization&scope=clips%3Aedit%20user%3Aread%3Aemail%20user%3Aread%3Asubscriptions`,
+                    icon: LockClosedIcon,
+                  }}
+                />
+              </div>
+              <div className="p-4">
+                <VerticalNavigation items={lists} />
+              </div>
             </div>
-            <div className="p-4">
-              <VerticalNavigation items={lists} />
-            </div>
-          </div>
-        </Popover.Panel>
-      </Transition>
-    </Popover>
-    )
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    );
   }
 
   const profileImage = () => {
     return (
-      <Image className="border rounded-lg" src={me.profileImageUrl} width={128} height={128} alt="" />
-    )
-  }
+      <Image
+        src={me.profileImageUrl}
+        width={128}
+        height={128}
+        alt={`${me.displayName}님의 프로필 사진`}
+        className="border rounded-lg"
+      />
+    );
+  };
 
   return (
     <Popover className="relative flex flex-col">
-      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+      <Popover.Button className="gap-x-1 text-sm font-semibold leading-6 text-gray-900">
         <Image src={me.profileImageUrl} className="rounded-full border" width={32} height={32} alt="" />
       </Popover.Button>
       <Transition
@@ -77,17 +94,16 @@ export default function MenuComponent({ me }: { me: IdentityType | undefined }) 
         enterTo="opacity-100 translate-y-0"
         leave="transition ease-in duration-150"
         leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
+        leaveTo="opacity-0 translate-y-1">
         <Popover.Panel className="absolute right-0 mt-10 w-screen max-w-sm">
           <div className="w-full overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
             <div className="p-4 border-b">
               <PopoverButton
                 button={{
                   name: me.displayName,
-                  description: '내 프로필 보기',
+                  description: "내 프로필 보기",
                   href: `/profile/${me.twitchUserId}`,
-                  icon: profileImage
+                  icon: profileImage,
                 }}
               />
             </div>
@@ -98,5 +114,5 @@ export default function MenuComponent({ me }: { me: IdentityType | undefined }) 
         </Popover.Panel>
       </Transition>
     </Popover>
-  )
+  );
 }
--- components/layouts/default/DefaultHeader.tsx
+++ components/layouts/default/DefaultHeader.tsx
@@ -1,124 +1,29 @@
-'use client';
-import React, { useState } from 'react';
-import Image from 'next/image'
-import Link from 'next/link'
-import dynamic from 'next/dynamic'
-import { Dialog } from '@headlessui/react'
-import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid'
-import { LockClosedIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import Bar3Menu from './Bar3Menu'
-import PopoverButton from '@/components/tailwind/PopoverButton'
-import VerticalNavigation from '@/components/tailwind/Navigation/VerticalNavigation'
-import type { IdentityType } from 'type'
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import { cookies } from "next/headers";
+import { getIdentity } from "@/services/auth";
+import Bar3Menu from "./Bar3Menu";
 
-const LiveBadge = dynamic(() => import('@/components/LiveBadge'))
+const LiveBadge = dynamic(() => import("@/components/LiveBadge"));
 
-const TwitchIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-6 h-6 text-gray-600 group-hover:text-indigo-600" viewBox="0 0 24 24">
-      <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714Z" clipRule="evenodd" />
-    </svg>
-  )
-}
-
-function myProfile(me: IdentityType | undefined) {
-  if(!me) {
-    return (
-      <PopoverButton
-        button={{
-          name: '트위치로 로그인',
-          description: '로그인 하고 클립 만들기',
-          href: `https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=0373yf8vzqpo4f9ln4ajqrq9fim3hd&redirect_uri=${process.env.NEXT_PUBLIC_APP_PROTOCOL}://${process.env.NEXT_PUBLIC_APP_HOST}api/authorization&scope=clips%3Aedit%20user%3Aread%3Aemail%20user%3Aread%3Asubscriptions`,
-          icon: LockClosedIcon
-        }}
-      />
-    )
-  }
-
-  const profileImage = () => {
-    return (
-      <Image className="border rounded-lg" src={me.profileImageUrl} width={128} height={128} alt="" />
-    )
-  }
-
-  return (
-    <PopoverButton
-      button={{
-        name: me.displayName,
-        description: '내 프로필 보기',
-        href: `/profile/${me?.twitchUserId}`,
-        icon: profileImage
-      }}
-    />
-  )
-}
-
-const lists = [
-  { name: '김나성 트위치', description: undefined, href: 'https://twitch.tv/naseongkim', icon: TwitchIcon },
-  { name: '김나성 트게더', description: undefined, href: 'https://tgd.kr/s/naseongkim', icon: ChatBubbleLeftRightIcon }
-]
-
-export default function DefaultHeader({ me }: {
-  me: IdentityType | undefined
-}) {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+export default async function DefaultHeader() {
+  const token = cookies().get("authorization");
+  const me = await getIdentity(token);
 
   return (
     <header className="bg-white/80 backdrop-blur-xl border-b sticky top-0 z-10">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 py-3">
         <div className="flex items-center gap-x-3">
           <Link href="/" className="flex items-start -m-1.5 p-1.5">
-            <span className="text-2xl text-black">na.<b>clip</b></span>
-            <span className='text-sm font-black text-blue-600 ml-1 mt-0.5'>NEXT</span>
+            <span className="text-2xl text-black">
+              na.<b>clip</b>
+            </span>
+            <span className="text-sm font-black text-blue-600 ml-1 mt-0.5">NEXT</span>
           </Link>
           <LiveBadge />
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 h-8 w-8"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">메뉴 열기</span>
-            <Bars3Icon className="shrink-0 h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="hidden lg:flex">
-          <Bar3Menu me={me} />
-        </div>
+        <Bar3Menu me={me} />
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 px-6 py-3">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-x-3 sm:opacity-0 sm:pointer-events-none">
-              <Link href="/" className="-m-1.5 p-1.5">
-                <span className="text-2xl text-black">
-                  na.<b>clip</b>
-                </span>
-              </Link>
-            </div>
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 h-8 w-8"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">메뉴 닫기</span>
-              <XMarkIcon className="shrink-0 h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="py-6">
-                {myProfile(me)}
-              </div>
-              <div className="space-y-2 py-6">
-                <VerticalNavigation items={lists} />
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
-  )
+  );
 }
