761db0d83b6a81f95eba3b8e0b5e8fcdabef9929
ASTERISK Kwon <hello@asterisk.moe>
2023-08-18 10:52:56 +0900

    refactor(api): 사용하지 않는 api 제거

--- app/api/twitch/stream/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getTwitchStream } from '@/services/stream';
-
-export async function GET() {
-  const stream = await getTwitchStream();
-  if(!stream) return NextResponse.json([]);
-
-  return NextResponse.json(stream.data[0]);
-}
