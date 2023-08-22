5bd2fe755d8c1466db5d4c710ac1ee4ad6400dd6
ASTERISK Kwon <hello@asterisk.moe>
2023-08-22 12:53:13 +0900

    type: UserType 추가 대응

--- services/users.ts
+++ services/users.ts
@@ -1,4 +1,4 @@
-import { ClipListsType, IdentityType } from "type";
+import { ClipListsType, UserType } from "type";
 
 export const getUserDetail = async (user?: number) => {
   if (!process.env.NEXT_PUBLIC_API_URL) throw new Error("NEXT_PUBLIC_API_URL is not defined.");
@@ -8,8 +8,7 @@ export const getUserDetail = async (user?: number) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/detail?id=${user}`);
 
   if (!res.ok) return undefined;
-  // @TODO: IdentityType과 UserDetailType 분리
-  return res.json() as Promise<IdentityType>;
+  return res.json() as Promise<UserType>;
 };
 
 export const getUserClips = async (user: number, offset: number) => {
