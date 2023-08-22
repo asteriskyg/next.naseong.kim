32bc742b049ee04d55a187c5f1a177a73b529bfd
ASTERISK Kwon <hello@asterisk.moe>
2023-08-22 16:58:26 +0900

    Storybook (#5)
    * build: 패키지 매니저 변경 (npm -> yarn)
    
    * build: 패키지 매니저 변경 (npm -> yarn)
    
    * feat: Storybook + TailwindCSS 설정
    
    * ci: add chromatic
    
    * storybook: ClipItem 생성
    
    * chore: gitignore 추가
    
    * revert: GitHub Actions 삭제 (chromatic)
    
    * chore: 필요 없는 스토리 삭제
    
    * chore: 필요 없는 스토리 삭제
    
    * build: 패키지 업데이트

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,18 @@
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import path from "path";
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {},
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -17,6 +20,17 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    if (!config.resolve) return config;
+    config.resolve.plugins = config.resolve.plugins || [];
+
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    );
+    return config;
   },
 };
 export default config;
--- stories/ClipItem.stories.ts
+++ stories/ClipItem.stories.ts
@@ -1,30 +1,29 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { ClipItem } from "../components/ClipItem";
+import { ClipItem } from "@/components/ClipItem";
 
-const meta: Meta<typeof ClipItem> = {
+const meta = {
   title: "ClipItem",
   component: ClipItem,
-  // ...
-};
+} satisfies Meta<typeof ClipItem>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const clip = {
+  contentId: "2e9d2fca549ee30046f9d56dcc69ea8f",
+  contentName: "✈ 코리안 조커 씨랙님께 탈콥배우기 ✈ (!다시보기)",
+  gameId: 491931,
+  streamStartedAt: new Date("2023-01-18T11:56:29Z"),
+  creatorId: 200469036,
+  creatorName: "덤덤이",
+  clipCreatedAt: new Date("2023-01-18T13:39:07.629Z"),
+  clipDuration: 90,
+  clipLastEdited: new Date("2023-01-18T13:39:07.629Z"),
+  clipName: "TastyFragileEchidnaSuperVinlin-Iu4ZcJpnrhqQsqer",
+  gameName: "Escape from Tarkov",
+};
+
 export const WithClip: Story = {
-  args: {
-    clip: {
-      clipCreatedAt: new Date(),
-      clipDuration: 90,
-      clipLastEdited: new Date(),
-      contentId: "string",
-      contentName: "string",
-      creatorId: 1,
-      creatorName: "string",
-      gameId: 1,
-      gameName: "string",
-      streamStartedAt: new Date(),
-      clipName: "string",
-    },
-  },
+  args: { clip },
 };
