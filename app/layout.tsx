8c80040b26bcf1250748b1a0e39c8d069ddbce0b
ASTERISK Kwon <hello@asterisk.moe>
2023-09-06 06:13:19 +0900

    fix: ts(71002) 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 
-import React from "react";
+import { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/react";
 
 import { DefaultFooter } from "@/components/layouts/default/DefaultFooter";
 import { DefaultHeader } from "@/components/layouts/default/DefaultHeader";
 
-export const RootLayout = ({ children }: { children: React.ReactNode }) => {
+export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <body className="bg-white dark:bg-twitch-dark">
@@ -17,6 +17,4 @@ export const RootLayout = ({ children }: { children: React.ReactNode }) => {
       </body>
     </html>
   );
-};
-
-export default RootLayout;
+}
