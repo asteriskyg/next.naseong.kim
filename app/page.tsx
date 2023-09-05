a88a2cfe6d46a4b3e611c2ebb65a7e0c409ba0a1
ASTERISK Kwon <hello@asterisk.moe>
2023-09-06 06:59:11 +0900

    feat: 헤더 위치 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
 import { getRecentClips } from "@/services/clips";
 
+import { DefaultFooter } from "@/components/layouts/default/DefaultFooter";
+import { DefaultHeader } from "@/components/layouts/default/DefaultHeader";
 import { RecentClips } from "@/components/RecentClips";
 
 export default async function Index() {
   const clips = await getRecentClips(0);
 
   return (
-    <div className="py-10">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6">
-        <h1 className="text-3xl font-bold leading-tight tracking-tight text-black dark:text-slate-200 suite">
-          클립 목록
-        </h1>
+    <>
+      <DefaultHeader />
+      <div className="py-10">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6">
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-black dark:text-slate-200 suite">
+            클립 목록
+          </h1>
+        </div>
+        <div className="mx-auto max-w-7xl py-8 px-6">
+          <RecentClips initialClips={clips} />
+        </div>
       </div>
-      <div className="mx-auto max-w-7xl py-8 px-6">
-        <RecentClips initialClips={clips} />
-      </div>
-    </div>
+      <DefaultFooter />
+    </>
   );
 }
