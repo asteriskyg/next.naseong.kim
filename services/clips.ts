8db7b8f00b629222d0ff82fde9b8d26f72985343
ASTERISK Kwon <hello@asterisk.moe>
2023-09-07 16:11:55 +0900

    feat: createClip 추가

--- services/clips.ts
+++ services/clips.ts
@@ -1,25 +1,44 @@
 import { ClipListsType, ClipType } from "type";
 
 export const getClipDetail = async (clip: string) => {
-  if (!process.env.NEXT_PUBLIC_API_URL) throw new Error("NEXT_PUBLIC_API_URL is not defined.");
+  if (!process.env.NEXT_PUBLIC_API_URL)
+    throw new Error("NEXT_PUBLIC_API_URL is not defined.");
 
   if (!clip) return undefined;
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clip/detail?id=${clip}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/clip/detail?id=${clip}`
+  );
 
   if (!res.ok) return undefined;
   return res.json() as Promise<ClipType>;
 };
 
 export const getRecentClips = async (offset: number) => {
-  if (!process.env.NEXT_PUBLIC_API_URL) throw new Error("NEXT_PUBLIC_API_URL is not defined.");
+  if (!process.env.NEXT_PUBLIC_API_URL)
+    throw new Error("NEXT_PUBLIC_API_URL is not defined.");
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clip/recent?offset=${offset}`, {
-    next: {
-      revalidate: 60,
-    },
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/clip/recent?offset=${offset}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
 
   if (!res.ok) return undefined;
   return res.json() as Promise<ClipListsType>;
+};
+
+export const createClip = async () => {
+  if (!process.env.NEXT_PUBLIC_API_URL)
+    throw new Error("NEXT_PUBLIC_API_URL is not defined.");
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/clip/create`, {
+    credentials: "include",
+  });
+
+  if (!res.ok) return undefined;
+  return res.json() as Promise<ClipType>;
 };
