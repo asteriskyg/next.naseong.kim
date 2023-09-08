506b94e9a459b02bf10af9e57a45a636c1aea594
ASTERISK Kwon <hello@asterisk.moe>
2023-09-08 17:23:03 +0900

    fix: 방송 상태 캐시 제거

--- services/stream.ts
+++ services/stream.ts
@@ -11,7 +11,7 @@ export const getTwitchStream = async () => {
   if (!token) return undefined;
 
   const res = await fetch(`https://api.twitch.tv/helix/streams?user_id=${process.env.TWITCH_BROADCASTER_ID}`, {
-    next: { revalidate: 300 },
+    next: { revalidate: 0 },
     headers: {
       Authorization: `Bearer ${token.access_token}`,
       "Client-Id": process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
