6b47004a65efc6b928c8c909e231c3e41025c86a
ASTERISK Kwon <hello@asterisk.moe>
2023-09-16 10:48:28 +0900

    feat: 트위치 토큰 갱신 방법 개선

---
+++ services/revalidate.ts
@@ -0,0 +1,14 @@
+export const revalidateByToken = async (tag: string) => {
+  if (!process.env.NEXT_PUBLIC_APP_URL)
+    throw new Error("NEXT_PUBLIC_APP_URL is not defined.");
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/revalidate?tag=${tag}`,
+    {
+      method: "POST",
+    }
+  );
+
+  if (!res.ok) return undefined;
+  return res.json();
+};
