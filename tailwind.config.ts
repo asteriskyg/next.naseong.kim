1d3631879477527375bda80a279468815cc81f16
ASTERISK Kwon <hello@asterisk.moe>
2023-09-16 14:01:49 +0900

    build: prettier-plugin-tailwind 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'twitch-dark': '#18181b',
-        'twitch-purple': '#9146ff',
+        "twitch-dark": "#18181b",
+        "twitch-purple": "#9146ff",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
+    // eslint-disable-next-line global-require
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
