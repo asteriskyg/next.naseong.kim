6ce0b8bf93b1ec78d49e2029238636790a991942
ASTERISK Kwon <hello@asterisk.moe>
2023-09-17 05:19:33 +0900

    style: 코드 블록 이동

--- services/auth.ts
+++ services/auth.ts
@@ -3,23 +3,6 @@ import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { revalidateByToken } from "@/services/revalidate";
 import type { IdentityType, TwitchClientCredentialsType } from "type";
 
-export const getServiceToken = async (code: string | null) => {
-  if (!process.env.NEXT_PUBLIC_API_URL)
-    throw new Error("NEXT_PUBLIC_API_URL is not defined.");
-
-  if (!code) return undefined;
-
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/auth/login?code=${code}`,
-  );
-
-  if (!res.ok) return undefined;
-  return (await res.json()) as Promise<{
-    access: string;
-    refresh: string;
-  }>;
-};
-
 export const getTwitchAccessToken = async () => {
   if (!process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID)
     throw new Error("TWITCH_CLIENT_ID is not defined.");
@@ -50,6 +33,23 @@ export const getTwitchAccessToken = async () => {
     if (!token.ok) return undefined;
     return (await token.json()) as Promise<TwitchClientCredentialsType>;
   }
+};
+
+export const getServiceToken = async (code: string | null) => {
+  if (!process.env.NEXT_PUBLIC_API_URL)
+    throw new Error("NEXT_PUBLIC_API_URL is not defined.");
+
+  if (!code) return undefined;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/login?code=${code}`,
+  );
+
+  if (!res.ok) return undefined;
+  return (await res.json()) as Promise<{
+    access: string;
+    refresh: string;
+  }>;
 };
 
 export const getIdentity = async (token?: RequestCookie) => {
