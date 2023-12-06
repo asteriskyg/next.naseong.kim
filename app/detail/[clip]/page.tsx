49b90d020e5c744f7dba06272b629564f9b826d6
ASTERISK <hello@asterisk.moe>
2023-12-06 14:32:03 +0900

    style: 로딩 아이콘 변경

--- app/detail/[clip]/page.tsx
+++ app/detail/[clip]/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { cookies } from "next/headers";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 import { getIdentity } from "@/services/auth";
 import { getUserDetail } from "@/services/users";
@@ -79,26 +80,7 @@ export default async function ClipDetail({
         </div>
         {!isClipActivated ? (
           <div className="sm:text-md text-dark mx-6 mt-6 flex flex-col items-start rounded-3xl bg-orange-100 p-4 dark:bg-yellow-800/50 dark:text-slate-200 sm:mx-0 sm:text-lg md:flex-row md:items-center">
-            <svg
-              className="mb-3 mr-3 h-6 w-6 animate-spin text-orange-500 dark:text-yellow-600 md:mb-0"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              />
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              />
-            </svg>
+            <ArrowPathIcon className="mr-3 h-6 w-6 animate-spin text-orange-500 dark:text-yellow-600 md:mb-0" />
             <div className="flex flex-wrap gap-x-1">
               <span>아직 동영상을 처리하고 있어요.</span>
               <span>클립이 보이지 않거나, 화질이 나쁠 수 있어요.</span>
